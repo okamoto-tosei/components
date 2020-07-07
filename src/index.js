549db7547d7047696b9d7181ba47e4d3d75860b6
oka! <66175301+okamoto-tosei@users.noreply.github.com>
2020-07-08 00:30:29 +0900

    ランダムプロフィールの作成

--- src/index.js
+++ src/index.js
@@ -1,17 +1,30 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import faker from 'faker';
+
+const App = () => {
+    return (
+        <div className='ui container comments'>
+            <div className='comment'>
+                <a href='/' className='avatar'>
+                    <img alt='avatar' src={faker.image.avatar()} />
+                </a>
+                <div className='content'>
+                    <a href='/' className='author'>
+                        Sam
+                    </a>
+                    <div className='metadata'>
+                        <span className='data'>Today at 6:00PM</span>
+                    </div>
+                    <div className='text'>Nice blog post!</div>
+                </div>
+            </div>
+        </div>
+    );
+};
+
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+    <App />,
+    document.querySelector('#root')
+)
